53e72c79aea56ad819242eed8d37e429be5e28bc
Vijay Sundaram <vijay.sundaram@gmail.com>
2016-01-20 01:20:42 -0800

    Update process.env references to work in Heroku

--- dotbot.js
+++ dotbot.js
@@ -3,12 +3,20 @@ var Botkit = require('botkit');
 
 // Botkit-based Redis store
 var Redis_Store = require('./redis_storage.js');
-var redis_url = "redis://127.0.0.1:6379"
+var redis_url = process.env.REDIS_URL ||"redis://127.0.0.1:6379"
 var redis_store = new Redis_Store({url: redis_url});
 
-require('./env.js');
+// Programmatically use appropriate process environment variables
+try {
+  require('./env.js');
+} catch (e) {
+  if (e.code === 'MODULE_NOT_FOUND') {
+    console.log('Not using environment variables from env.js');
+  }
+}
+var port = process.env.PORT || process.env.port;
 
-if (!process.env.clientId || !process.env.clientSecret || !process.env.port) {
+if (!process.env.clientId || !process.env.clientSecret || !port) {
   console.log('Error: Specify clientId clientSecret and port in environment');
   process.exit(1);
 }
@@ -24,7 +32,7 @@ var controller = Botkit.slackbot({
   }
 );
 
-controller.setupWebserver(process.env.port,function(err,webserver) {
+controller.setupWebserver(port,function(err,webserver) {
 
   webserver.get('/',function(req,res) {
     res.sendFile('index.html', {root: __dirname});
